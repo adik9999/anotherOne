169d516efabe8912b633cd24cbdef7634fc66e4a
adik999 <kumar.aditya9999@gmail.com>
2022-04-25 11:56:12 +0530

    maybe

--- main.js
+++ main.js
@@ -2,21 +2,46 @@ $(".sec").css("cursor", "pointer");
 $(".seb").css("cursor", "pointer");
 
 $(".seb").click(function() {
-  var aud = new Audio("click_sound.wav");
+  var aud = new Audio("punch.mp3");
   aud.play();
 });
+$(".seb").mouseover(function(){
+  var aud=new Audio("watson.mp3");
+  aud.play();
+});
+$(".sec").mouseover(function(){
+  var aud=new Audio("watson.mp3");
+  aud.play();
+});
+$(".fa-chess").mouseover(function(){
+  var aud=new Audio("watson.mp3");
+  aud.play();
+});
+$(".btn-ban").mouseover(function(){
+  var aud=new Audio("watson.mp3");
+  aud.play();
+});
+$(".fa-chess").click(function(){
+  var aud=new Audio("punch.mp3");
+  aud.play();
+});
+$(".btn-ban").click(function(){
+  var aud=new Audio("punch.mp3");
+  aud.play();
+});
+
 $(".sec").click(function() {
-  var aud = new Audio("click_sound.wav");
+  var aud = new Audio("punch.mp3");
   aud.play();
 });
-$(".rand-img").click(function() {
-  $(".rand-img").addClass("animate-piece");
-  var aud = new Audio("woosh.wav");
-  aud.play();
-  setTimeout(function() {
-    $(".rand-img").removeClass("animate-piece");
-  }, 2000);
-});
+// $(".rand-img").click(function() {
+//   $(".rand-img").addClass("animate-piece");
+//   var aud = new Audio("woosh.wav");
+//   aud.play();
+//   setTimeout(function() {
+//     $(".rand-img").removeClass("animate-piece");
+//   }, 2000);
+// });
 $(".btnn").click(function() {
   var aud = new Audio("gameOn.wav");
   aud.play();
